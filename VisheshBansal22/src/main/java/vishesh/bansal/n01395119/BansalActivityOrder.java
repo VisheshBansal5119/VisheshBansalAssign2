cbfb74e5a3b7b67976de9995970bc0f522331b7a
Vishesh <visheshbansal369@gmail.com>
2021-06-15 12:43:33 -0400

    Added Payment Page

--- VisheshBansal22/src/main/java/vishesh/bansal/n01395119/BansalActivityOrder.java
+++ VisheshBansal22/src/main/java/vishesh/bansal/n01395119/BansalActivityOrder.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class BansalActivityOrder extends AppCompatActivity {
@@ -17,5 +19,10 @@ public class BansalActivityOrder extends AppCompatActivity {
         String storeName = intent.getStringExtra(VisheshActivity.EXTRA_MESSAGE);
         TextView storeTitle = findViewById(R.id.VisheshStoreName);
         storeTitle.setText(storeName);
+    }
+    public void onCheckout(View view){
+        Intent intent = null;
+        intent = new Intent(this,BansalActivityPayment.class);
+        startActivity(intent);
     }
 }
---
+++ VisheshBansal22/src/main/java/vishesh/bansal/n01395119/BansalActivityPayment.java
@@ -0,0 +1,17 @@
+package vishesh.bansal.n01395119;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class BansalActivityPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bansal_payment);
+        Intent intent = getIntent();
+
+    }
+}
